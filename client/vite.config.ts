3e8c81954b2e389bd152f78e5418da4677f1c2c7
Marius Vizbras <marius.viz@gmail.com>
2024-08-04 11:34:50 +0300

    1.1.0 Playwrith

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+      // Assuming you have a setup to share types or utilities
+      '@shared': path.resolve(__dirname, '../shared'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        // Externalize server-only modules here
+        '@server/utils/validation',
+      ],
     },
   },
 })
